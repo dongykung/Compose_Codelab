045df619115c3da9906c05494818a26aa9fc6551
dongykung <92030316+dongykung@users.noreply.github.com>
2024-01-20 21:32:42 +0900

    commit - Woof Animation code(codelab)

--- app/src/main/java/com/example/android_composecamp_codelab/Chapter3/Woof.kt
+++ app/src/main/java/com/example/android_composecamp_codelab/Chapter3/Woof.kt
@@ -1,27 +1,45 @@
 package com.example.android_composecamp_codelab.Chapter3
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -69,34 +87,89 @@ fun WoofApp() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun WoofTopAppBar(modifier: Modifier = Modifier) {
+
     CenterAlignedTopAppBar(
         title = {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Image(painter = painterResource(id = R.drawable.ic_woof_logo), contentDescription = null,
-                    modifier= Modifier
+                Image(
+                    painter = painterResource(id = R.drawable.ic_woof_logo),
+                    contentDescription = null,
+                    modifier = Modifier
                         .size(dimensionResource(id = R.dimen.image_size))
-                        .padding(dimensionResource(id = R.dimen.padding_small)))
-                Text(text = "Woof",
-                    style = MaterialTheme.typography.displayLarge)
+                        .padding(dimensionResource(id = R.dimen.padding_small))
+                )
+                Text(
+                    text = "Woof",
+                    style = MaterialTheme.typography.displayLarge
+                )
             }
         },
         modifier = modifier
     )
 }
 
+@SuppressLint("UnusedCrossfadeTargetStateParameter")
 @Composable
 private fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
+    val color by animateColorAsState(
+        targetValue = if (expanded) MaterialTheme.colorScheme.tertiaryContainer
+        else MaterialTheme.colorScheme.primaryContainer, label = ""
+    )
+
     Card(modifier = modifier) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(id = R.dimen.padding_small))
-        ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy, //스프링의 탄성
+                        stiffness = Spring.StiffnessMedium  //종료값으로 이동하는 속도 정의
+                    )
+                )
+                .background(color)
+        ) {//항목 높이 변경을 애니메이션 처리
+            //앱의 목록 항목 높이를 애니메이션으로 보여준다 - > animationSpec 매개변수 사용
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(modifier = Modifier.weight(1f))
+                DogItemButton(expanded = expanded, onClick = { expanded = !expanded })
+            }
+            //애니메이션 방법 1
+//            Crossfade(targetState = expanded, label = "") {
+//                when(it){
+//                    true->{  DogHobby(
+//                        dogHobby = dog.hobbies,
+//                        modifier = Modifier.padding(
+//                            start = dimensionResource(id = R.dimen.padding_medium),
+//                            top = dimensionResource(id = R.dimen.padding_small),
+//                            end = dimensionResource(id = R.dimen.padding_medium),
+//                            bottom = dimensionResource(id = R.dimen.padding_medium)
+//                        )
+//                    )}
+//                    false->{}
+//                }
+//
+//            }
+            if (expanded) {
+                DogHobby(
+                    dogHobby = dog.hobbies,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(id = R.dimen.padding_medium),
+                        top = dimensionResource(id = R.dimen.padding_small),
+                        end = dimensionResource(id = R.dimen.padding_medium),
+                        bottom = dimensionResource(id = R.dimen.padding_medium)
+                    )
+                )
+            }
         }
     }
 }
@@ -117,6 +190,21 @@ private fun DogIcon(
 }
 
 @Composable
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = { onClick() }, modifier = modifier) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            contentDescription = stringResource(id = R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+@Composable
 private fun DogInformation(
     @StringRes dogName: Int,
     dogAge: Int,
@@ -130,6 +218,23 @@ private fun DogInformation(
         )
         Text(
             text = stringResource(R.string.years_old, dogAge),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Composable
+private fun DogHobby(
+    @StringRes dogHobby: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(id = R.string.about),
+            style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            text = stringResource(id = dogHobby),
             style = MaterialTheme.typography.bodyLarge
         )
     }
